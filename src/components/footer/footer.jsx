c6c22c08e738ee073187f23c93b5e76dacf2c891
jiwon Nam <swm10099@naver.com>
2022-01-06 04:55:23 +0900

    login component

---
+++ src/components/footer/footer.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styles from "./footer.module.css";
+
+const Footer = (props) => (
+  <footer className={styles.footer}>
+    <p className={styles.title}>Make your business card</p>
+  </footer>
+);
+
+export default Footer;
