a89f1dd05cd99f4c3440ee1f78546959cdb42e72
jiwon Nam <swm10099@naver.com>
2022-01-09 04:58:07 +0900

    add edit form and preview

--- src/components/maker/maker.jsx
+++ src/components/maker/maker.jsx
@@ -13,17 +13,19 @@ const Maker = ({ authService }) => {
       id: "1",
       name: "jiwon",
       company: "Samsung",
-      theme: "SoftWare Engineer",
+      theme: "light",
+      title: "SoftWare Engineer",
       email: "njw10099@gmail.com",
       message: "go for it",
       fileName: "jiwon",
-      fileURL: "jiwon.png",
+      fileURL: null,
     },
     {
       id: "2",
       name: "jiwon2",
       company: "Samsung",
-      theme: "SoftWare Engineer",
+      theme: "dark",
+      title: "SoftWare Engineer",
       email: "njw10099@gmail.com",
       message: "go for it",
       fileName: "jiwon",
@@ -33,11 +35,12 @@ const Maker = ({ authService }) => {
       id: "3",
       name: "jiwon3",
       company: "Samsung",
-      theme: "SoftWare Engineer",
+      theme: "colorful",
+      title: "SoftWare Engineer",
       email: "njw10099@gmail.com",
       message: "go for it",
       fileName: "jiwon",
-      fileURL: "jiwon.png",
+      fileURL: null,
     },
   ]);
 
@@ -54,11 +57,16 @@ const Maker = ({ authService }) => {
     });
   });
 
+  const addCard = (card) => {
+    const updated = [...cards, card];
+    setCards(updated);
+  };
+
   return (
     <section className={styles.maker}>
       <Header onLogout={onLogout} />
       <div className={styles.container}>
-        <Editor cards={cards} />
+        <Editor cards={cards} addCard={addCard} />
         <Preview cards={cards} />
       </div>
       <Footer />
